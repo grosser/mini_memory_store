d60a7ce5e71ddb86bde4c5ff4a71bb4f40e1f9f1
grosser <grosser.michael@gmail.com>
2010-12-22 08:38:58 +0100

    initial working version

--- lib/mini_memory_store.rb
+++ lib/mini_memory_store.rb
@@ -1,3 +1,43 @@
 class MiniMemoryStore
   VERSION = File.read( File.join(File.dirname(__FILE__),'..','VERSION') ).strip
+
+  def initialize(options={})
+    @options = options
+    @last_set = 0
+  end
+
+  def set(value)
+    @last_set = Time.now.to_i
+    @value = value
+  end
+
+  def get
+    if expired?
+      nil
+    else
+      @value
+    end
+  end
+
+  def cache(&block)
+    value = get
+    if value.nil?
+      value = block.call
+      set(value)
+      value
+    else
+      value
+    end
+  end
+
+  def clear
+    set nil
+  end
+
+  private
+
+  def expired?
+    return unless @options[:expires_in]
+    (Time.now.to_i - @options[:expires_in]) >= @last_set
+  end
 end
--- spec/mini_memory_store_spec.rb
+++ spec/mini_memory_store_spec.rb
@@ -1,7 +1,59 @@
 require File.expand_path('spec/spec_helper')
 
 describe MiniMemoryStore do
+  let(:store){ MiniMemoryStore.new }
+
   it "has a VERSION" do
     MiniMemoryStore::VERSION.should =~ /^\d+\.\d+\.\d+$/
+  end
+
+  it "is nil when not set" do
+    store.get.should == nil
+  end
+
+  it "can store a value" do
+    store.set 1
+    store.get.should == 1
+  end
+
+  it "can cache a value" do
+    store.cache{1}.should == 1
+    store.cache{2}.should == 1
+  end
+
+  it "can cache false" do
+    store.cache{false}.should == false
+    store.cache{1}.should == false
+  end
+
+  it "does not cache nil" do
+    store.cache{nil}.should == nil
+    store.cache{1}.should == 1
+  end
+
+  it "clears" do
+    store.set 1
+    store.clear
+    store.get.should == nil
+  end
+
+  it "does not expire" do
+    store.set 1
+    sleep 1
+    store.get.should == 1
+  end
+
+  it "expires after given time" do
+    store = MiniMemoryStore.new(:expires_in => 1)
+    store.set 1
+    store.get.should == 1
+    sleep 1
+    store.get.should == nil
+  end
+
+  it "expires immediately with expires_in => 0" do
+    store = MiniMemoryStore.new(:expires_in => 0)
+    store.set 1
+    store.get.should == nil
   end
 end
